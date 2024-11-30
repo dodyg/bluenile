8764fc67f7d6fdb02eb44c539a435bbbc987a905
Dody Gunawinata <dodyg@silverkeytech.com>
2024-11-30 23:42:00 +0200

    Start nats integration

--- fire-hose-nats/Firehose.Web/Program.cs
+++ fire-hose-nats/Firehose.Web/Program.cs
@@ -1,6 +1,26 @@
+using NATS.Client.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddNatsClient(connectionName: "nats");
+builder.Services.AddHostedService<FirehoseService>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Anna");
+app.MapGet("/", async (INatsConnection conn) =>
+{
+    await conn.PublishAsync(subject: "foo", data: "Hello, World!");
+    return Results.Content("hello world");
+}); 
+
 
 app.Run();
+
+public class FirehoseService(INatsConnection conn): BackgroundService, IDisposable
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await foreach(var msg in conn.SubscribeAsync<string>(subject: "foo"))
+        {
+            Console.WriteLine(msg.Data);
+        };
+    }
+}
